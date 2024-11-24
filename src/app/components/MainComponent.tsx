1b0ac92d8da5f45dac230d250525c2177c321e9c
Naif Otibi <nn.n@outlook.cl>
2024-11-25 00:09:39 +0300

    first appearence of main wrapper

--- src/app/components/MainComponent.tsx
+++ src/app/components/MainComponent.tsx
@@ -2,5 +2,9 @@
 
 
 export default function MainComponent(){
-    
+    return (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            
+        </div>
+    )
 }
