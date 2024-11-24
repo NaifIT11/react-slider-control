276f5f19c229a9db0e6693ed7b0fcb8a25099d6b
Naif Otibi <nn.n@outlook.cl>
2024-11-25 00:24:46 +0300

    stylign the thumb

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -15,7 +15,7 @@ const Slider = forwardRef<
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-indigo-400">
       <SliderPrimitive.Range className="absolute h-full bg-indigo-400"  />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb  />
+    <SliderPrimitive.Thumb  className="block h-5 w-5 rounded-full border-2 border-indigo-300 bg-indigo-500 ring-offset-indigo-500 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"/>
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
