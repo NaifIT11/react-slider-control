0c45b449b31d256559ab4db262602d73389641dc
Naif Otibi <nn.n@outlook.cl>
2024-11-25 00:39:11 +0300

    fixing the styling

--- src/app/components/MainComponent.tsx
+++ src/app/components/MainComponent.tsx
@@ -7,7 +7,7 @@ import { Slider } from "./Slider";
 export default function MainComponent(){
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Slider defaultValue={[33]} min={0} max={100} step={1} />
+            <Slider defaultValue={[33]} min={0} max={100} step={1} aria-label="Slider Control" />
         </div>
     )
 }
--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -1,23 +1,23 @@
-import * as SliderPrimitive from "@radix-ui/react-slider"
-import { forwardRef } from "react"
+    import * as SliderPrimitive from "@radix-ui/react-slider"
+    import { forwardRef } from "react"
 
 
 
-const Slider = forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    {...props}
-    className="relative flex w-full touch-none items-center"
-  >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-500">
-      <SliderPrimitive.Range className="absolute h-full bg-indigo-400"  />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb  className="block h-5 w-5 rounded-full border-2 border-indigo-300 bg-indigo-500 ring-offset-indigo-500 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"/>
-  </SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
+    const Slider = forwardRef<
+    React.ElementRef<typeof SliderPrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+    >(({ className, ...props }, ref) => (
+    <SliderPrimitive.Root
+        ref={ref}
+        {...props}
+        className="relative flex w-[200px] h-[20px] touch-none items-center"
+    >
+        <SliderPrimitive.Track className="relative h-2 w-full grow rounded-full bg-indigo-200">
+        <SliderPrimitive.Range className="absolute rounded-full h-full bg-indigo-400"  />
+        </SliderPrimitive.Track>
+        <SliderPrimitive.Thumb  className="block h-5 w-5 cursor-pointer rounded-full border-2 border-indigo-300 bg-indigo-500 hover:bg-indigo-600 ring-offset-indigo-500 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"/>
+    </SliderPrimitive.Root>
+    ))
+    Slider.displayName = SliderPrimitive.Root.displayName
 
-export { Slider }
+    export { Slider }
